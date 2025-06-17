f6113f7074072989493e2d4f13a4fd6825cb78b9
Ryota Murakami <dojce1048@gmail.com>
2025-06-17 22:09:06 +0900

    feat: Update Storybook configuration and add Design System documentation
    - Updated Storybook configuration in .storybook/main.ts:
      - Refined story paths to focus on components directory.
      - Replaced @storybook/addon-onboarding with

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,28 @@
 import type { StorybookConfig } from '@storybook/nextjs'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/components/**/*.mdx',
+    '../src/components/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
-    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
     '@storybook/addon-links',
+    '@storybook/addon-docs',
     '@chromatic-com/storybook',
-    '@storybook/addon-docs'
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
   staticDirs: ['../public'],
+  typescript: {
+    check: false,
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
 }
 export default config
