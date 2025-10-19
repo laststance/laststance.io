0896d0785a499f61a43ed8eb42e61313a8e2c057
Ryota Murakami <dojce1048@gmail.com>
2025-10-19 23:46:53 +0900

    fix: remove experimental mdxRs configuration from Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,6 @@ const withBundleAnalyzer = withBundleAnalyzerOriginal({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    mdxRs: false,
-  },
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
